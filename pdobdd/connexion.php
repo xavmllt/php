f696bd36692431b5696f5b1fa26a6c09cf386202
xavmllt <xaviermaillot100@gmail.com>
2023-11-23 16:30:44 +0100

    suite du cours

--- pdobdd/connexion.php
+++ pdobdd/connexion.php
@@ -3,5 +3,17 @@ $dsn = "mysql:host=localhost;dbname=maBase";
 $user = "root";
 $password = "root";
 
-$cnx = new PDO($dsn, $user, $password);
+try{
+    $cnx = new PDO($dsn, $user, $password);
+}catch(PDOException $e){
+    echo "Une erreur est survenue !";
+};
+
+$sql = "SELECT * FROM clients";
+$rs_req = $cnx->query($sql);
+while($donnees = $rs_req->fetch()){
+    echo "<pre>";
+    print_r($donnees);
+    echo "</pre>";
+};
 ?>
--- pdobdd/notePdo.php
+++ pdobdd/notePdo.php
@@ -38,5 +38,61 @@
 
     Concllusion :
     Nous venons de créer un objet PDO pour nous connecter à notre base de données mysql.
+
+
+    Gestion d'une erreur éventuelle de connexion
+    Si pour une raison quelconque, une erreur de connexion à la base de données survenait, nous allons gérer cette erreur et non la subir. En effet, en cas d'erreur, mySQL renvoie un message d'erreur à notre base de données et nous allons modifier le login de la varaible $user et ajouter un mot de passe à notre variable $password, afin de générer une erreur de connexion. Puis nous initialisons l'objet PDO.
+*/
+    $dsn = "mysql:host=localhost;dbname=mabase";
+    $user = "toto";
+    $password = "tata";
+    $cnx = new PDO($dsn, $user, $password);
+/*
+    Ensuite nous lançons la page dans le browser. Nous nous attendons à une erreur, puisque le couple user/password n'est pas correct.
+    Nous pouvons observer une faille de sécurité indéniable. Le couple user/password apparait clairement.
+    Pour rémédier à cela, nous allons utiliser les blocs TRY/CATCH. 
+
+    *** Le couple TRY/CATCH 
+    Le couple TRY/CATCH va nous permettre de pouvoir gérer une erreur de connexion à la base de données et d'en personnaliser le message.
+    --- Principe de fonctionnement
+    TRY va tenter de se connecter à la base de données et si il y a une erreur, CATCH fera en sorte de renvoyer un message d'erreur que nous allons personnaliser. Cette erreur sera délcarée sous la forme d'une exception.
+*/
+    try{
+        $cnx = new PDO($dsn, $user, $password);
+    }catch(PDOException $e){
+        echo "Une erreur est survenue !";
+    };
+/*
+    Nous relançons la page dans notre navigateur et cette fois, le message d'erreur qui va s'afficher sera le suivant : "Une erreur est survenue !".
+
+    Conclusion :
+    Vous venez d'apprendre à vous connectez à une base de données en utilisant l'objet PDO de PHP.
+    nb : En cas de problème, tout ce qui se trouve à l'intérieur du TRY sera stoppé au profit de ce qui se trouve à l'intérieur du CATCH. Et si tout vas bien, alors seulement ce qui se trouve à l'intérieur du TRY sera exécuté.
+*/
+
+/*
+    AFFICHER DES SONNEES AVEC PDO
+
+    Ecriture de la requête
+
+    $sql = SELECT * FROM clients;
+
+    Exécution de la requête
+
+    Pour exécuter cette requête nous récupérons l'objet PDO et nous effectuons une requête query. Nous stockons cette requête dans une variable nommée $rs_req.
+*/
+    $rs_req = $cnx->query($sql);
+/*
+    Nous allons maintenant récupérer les donnéees de notre requête. Pour cela nous utilisons une boucle while ainsi que la méthode fetch(). On affecte à la variable $données, chaque enregistrement de la table clients.
+*/
+    while($donnees = $rs_req->fetch()){
+        echo "<pre>";
+        print_r($donnees);
+        echo "</pre>";
+    };
+/*
+    On obtient chaque ligne de notre table clients, rangée dans un tableau (Array) et chaque tableau (array) contient :
+        - un tableau associatif
+        - un tableau numérique
 */
 ?>
